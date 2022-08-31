4225b62678e0261d6188cfd7ea8a69650e91c08a
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-08-31 19:40:03 -0400

    commented out console log.

--- components/card/card.component.js
+++ components/card/card.component.js
@@ -1,7 +1,7 @@
 import { CardContainer } from './card.styles'
 
 const Card = ({ obj: { title, description, id, imageUrl } }) => {
-  console.log(title, description, id)
+  // console.log(title, description, id)
   return (
     <CardContainer image={imageUrl}>
       <div>{title}</div>
