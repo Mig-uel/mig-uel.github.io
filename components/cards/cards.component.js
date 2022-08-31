24da060bfd9fcfa52e4ce14460a136087ffce8a4
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-08-31 19:40:55 -0400

    integrated firebase firestore and removed hard code json file.

--- components/cards/cards.component.js
+++ components/cards/cards.component.js
@@ -1,12 +1,38 @@
-import Card from '../card/card.component'
+import { useState, useEffect, CSSProperties } from 'react'
 
-import projects from '../../projects.json'
+/* ---- COMPONENTS ---- */
+import Card from '../card/card.component'
+import { FadeLoader } from 'react-spinners'
+
+/* ---- UTILS ---- */
+import { getProjects } from '../../utils/firebase.utils'
 
 /* ------- STYLED COMPONENTS -------*/
 import { CardsContainer } from './cards.styles'
 
+const override = {
+  display: 'block',
+  margin: '0 auto',
+  marginTop: '100px',
+  position: 'relative',
+}
+
 const Cards = () => {
-  return (
+  const [projects, setProjects] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+
+    getProjects().then((i) => {
+      setProjects(i)
+      setLoading(false)
+    })
+  }, [])
+
+  return loading ? (
+    <FadeLoader color='#fff' loading={loading} cssOverride={override} />
+  ) : (
     <CardsContainer>
       {projects.map((project) => (
         <Card key={project.id} obj={project} />
