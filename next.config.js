238e27ccd49d18c997715543b7081f703299196e
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-12 19:56:00 -0400

    added new image domain to next config.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['miguelcastillo.net', 'localhost'],
+    domains: ['miguelcastillo.net', 'localhost', 'raw.githubusercontent.com'],
   },
   compiler: {
     styledComponents: true,
