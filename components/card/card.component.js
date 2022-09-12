9306ed8978931f8920b7282de07eb88517737635
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-12 09:59:06 -0400

    created a container to hold desc and title, preparing for dynamic icons.

--- components/card/card.component.js
+++ components/card/card.component.js
@@ -1,11 +1,31 @@
-import { CardContainer } from './card.styles'
+import { CardContainer, CardInfoContainer, TagsContainer } from './card.styles'
 
-const Card = ({ obj: { title, description, id, imageUrl } }) => {
+/* ---- React Icons ---- */
+import { TbApi, TbDeviceMobile } from 'react-icons/tb' // API, Mobile
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiRedux,
+  SiNextdotjs,
+  SiTypescript,
+  SiFirebase,
+  SiStyledcomponents,
+} from 'react-icons/si' // HTML5, CSS3,JS, React, Redux, NextJS, TypeScript, Firebase
+
+const Card = ({
+  obj: { title, description, id, imageUrl, demoUrl, githubUrl, tags },
+}) => {
   // console.log(title, description, id)
   return (
     <CardContainer image={imageUrl}>
-      <div>{title}</div>
-      <div>{description}</div>
+      <CardInfoContainer>
+        <div className='title'>{title}</div>
+        <div className='desc'>{description}</div>
+      </CardInfoContainer>
+
+      <TagsContainer></TagsContainer>
     </CardContainer>
   )
 }
