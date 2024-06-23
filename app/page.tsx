dbf57410cb30abb43df77d39311c9bc4b2869fed
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-23 15:14:42 -0400

    refactor: Update page layout and styling in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <section className='flex flex-row gap-10 justify-center content-center'>
-      <div className='flex flex-col gap-5 justify-center w-[400px] h-[400px'>
+    <section className='py-10 flex lg:flex-row justify-center gap-10  flex-col-reverse items-center'>
+      <div className='flex flex-col gap-5 justify-center w-[400px] lg:h-[400px]'>
         <h1 className='text-4xl'>HELLO,</h1>
         <h1 className='text-4xl'>I AM MIGUEL</h1>
         <p className='leading-7'>
           A front-end developer with some experience in back-end. Skilled in
-          JavaScript programming language and JaveScript frameworks, with a
+          JavaScript programming language and JavaScript frameworks, with a
           proven track record of developing successful applications. Able to
           work independently and as part of team, and comfortable with agile
           development methods.
