5d2ee8641d9e7a3ae6b4bbaae574f40e7aaf75d1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-06-26 18:39:41 -0400

    refactor: Update ProjectsPage component for improved layout and consistency

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,39 @@
-const ProjectsPage = () => {
-  return <></>
+import ProjectCard from '@/components/project-card/project-card.component'
+import { connectDB } from '@/config/db.config'
+import Project from '@/models/project.model'
+import { IProject } from '@/types'
+import Link from 'next/link'
+import { Key } from 'react'
+
+const getProjects = async (): Promise<IProject[]> => {
+  try {
+    connectDB()
+
+    const projects: IProject[] = await Project.find({}).lean()
+
+    if (!projects) throw new Error('Cannot fetch projects.')
+
+    return projects
+  } catch (error) {
+    console.log((error as Error).message)
+    return []
+  }
+}
+
+const ProjectsPage = async () => {
+  const projects = await getProjects()
+
+  return !projects || !projects.length ? (
+    <p className='text-center text-2xl'>No projects available.</p>
+  ) : (
+    <>
+      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4  my-30'>
+        {projects.map((project) => (
+          <ProjectCard key={project._id as string} project={project} />
+        ))}
+      </section>
+    </>
+  )
 }
 
 export default ProjectsPage
