78ac951b589d07d7ba4e8f754f4d21a709be703a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 18:49:28 -0400

    Refactor MenuButton component to improve code readability and maintainability

--- components/menu-button.component.tsx
+++ components/menu-button.component.tsx
@@ -8,11 +8,12 @@ const MenuButton = () => {
   const [isMenuActive, setIsMenuActive] = useState<boolean>(false)
 
   return (
-    <div className='absolute bottom-5 left-5 rounded-full shadow-lg'>
-      <Button size='icon' className='rounded-full w-12 h-12 md:w-14 md:h-14'>
-        <Menu className='w-5 h-5 md:w-12 md:h-6' />
-      </Button>
-    </div>
+    <Button
+      size='icon'
+      className='rounded-full w-12 h-12 md:w-14 md:h-14 absolute bottom-5 left-5  shadow-lg'
+    >
+      <Menu className='w-5 h-5 md:w-12 md:h-6' />
+    </Button>
   )
 }
 export default MenuButton
