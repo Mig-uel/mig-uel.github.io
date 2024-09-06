8e1bafaee5148a522a8545240b4272d7637666af
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 20:30:23 -0400

    Refactor menu component to dynamically render menu links using MenuContext

--- components/menu.component.tsx
+++ components/menu.component.tsx
@@ -1,10 +1,24 @@
 'use client'
 
+import { useContext } from 'react'
 import { MenuContext } from '@/app/_providers/providers'
 import Link from 'next/link'
-import { MouseEventHandler, useContext } from 'react'
+import { usePathname } from 'next/navigation'
+
+type MenuLinks = {
+  href: string
+  label: string
+}
+
+const menuLinks: MenuLinks[] = [
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/projects', label: 'Projects' },
+  { href: '/contact', label: 'Contact' },
+]
 
 const Menu = () => {
+  const path = usePathname()
   const { isMenuActive, handleMenuButtonClick } = useContext(MenuContext)
 
   return (
@@ -14,9 +28,17 @@ const Menu = () => {
       }`}
       onClick={handleMenuButtonClick}
     >
-      <Link href='/'>Home</Link>
-      <Link href='/about'>About</Link>
-      <Link href='/projects'>Projects</Link>
+      {menuLinks.map((link, index) => (
+        <Link
+          key={index}
+          href={link.href}
+          className={`hover:opacity-100 ${
+            path === link.href ? 'opacity-100' : 'opacity-50'
+          }`}
+        >
+          {link.label}
+        </Link>
+      ))}
     </div>
   )
 }
