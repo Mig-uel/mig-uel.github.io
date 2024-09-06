6783d6717f9c0b7d1aebfb1e40d9d32012dc83a9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 20:13:47 -0400

    Refactor MenuButton component to use MenuContext for handling menu button click

--- components/menu-button.component.tsx
+++ components/menu-button.component.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import { useState } from 'react'
 import { Menu } from 'lucide-react'
 import { Button } from './ui/button'
+import { useContext } from 'react'
+import { MenuContext } from '@/app/_providers/providers'
 
 const MenuButton = () => {
-  const [isMenuActive, setIsMenuActive] = useState<boolean>(false)
-
-  const handleClick = () => {}
+  const { handleMenuButtonClick } = useContext(MenuContext)
 
   return (
     <Button
+      onClick={handleMenuButtonClick}
       type='button'
       size='icon'
       className='rounded-full w-12 h-12 md:w-14 md:h-14 absolute bottom-5 left-5 shadow-lg'
