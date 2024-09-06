5b6efd6e8a1c4b1a8ef4c00d28f8105286ac3cb9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 20:14:01 -0400

    Refactor Menu component to use MenuContext for handling menu button click

--- components/menu.component.tsx
+++ components/menu.component.tsx
@@ -1,8 +1,20 @@
+'use client'
+
+import { MenuContext } from '@/app/_providers/providers'
 import Link from 'next/link'
+import { MouseEventHandler, useContext } from 'react'
 
 const Menu = () => {
+  const { isMenuActive, handleMenuButtonClick } = useContext(MenuContext)
+
   return (
-    <div className='h-screen w-full flex flex-col justify-center items-center absolute gap-5 uppercase font-medium bg-white z-1'>
+    <div
+      className={`text-lg md:text-xl h-screen w-full flex flex-col justify-center items-center absolute gap-5 uppercase font-medium bg-white ${
+        !isMenuActive && 'hidden'
+      }`}
+      onClick={handleMenuButtonClick}
+    >
+      <Link href='/'>Home</Link>
       <Link href='/about'>About</Link>
       <Link href='/projects'>Projects</Link>
     </div>
