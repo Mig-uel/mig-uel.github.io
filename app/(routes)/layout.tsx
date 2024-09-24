b05d884e4e3db5fb622f21671bf666ee5ffc72a6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-24 18:59:03 -0400

    Add layout to new (routes) folder

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Title from '@/components/title.component'
+import { usePathname } from 'next/navigation'
+
+const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname()
+
+  console.log(pathname)
+
+  return (
+    <section className='flex flex-col items-center py-10'>
+      <Title text={pathname} />
+      {children}
+    </section>
+  )
+}
+export default RoutesLayout
