7b6b7b78f7f2d82fa0bf87fb54d4c1b2d93369e7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-25 16:14:19 -0400

    Refactor loading component to use Skeleton for displaying a loading state in the projects route

--- app/(routes)/projects/loading.tsx
+++ app/(routes)/projects/loading.tsx
@@ -1,4 +1,23 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
 const Loading = () => {
-  return <div>Loading...</div>
+  return (
+    <>
+      <div className='w-full mt-10 flex flex-col justify-center items-center gap-y-1'>
+        <Skeleton className='min-w-[350px] w-[650px]  h-6' />
+        <Skeleton className='min-w-[250px] w-[550px]  h-6' />
+      </div>
+
+      <div className='mt-20 grid md:grid-cols-3 gap-y-10 justify-center'>
+        <Skeleton className='flex flex-col items-center justify-center w-[450px] h-[250px] opacity-80 hover:opacity-100 group relative' />
+        <Skeleton className='flex flex-col items-center justify-center w-[450px] h-[250px] opacity-80 hover:opacity-100 group relative' />
+        <Skeleton className='flex flex-col items-center justify-center w-[450px] h-[250px] opacity-80 hover:opacity-100 group relative' />
+        <Skeleton className='flex flex-col items-center justify-center w-[450px] h-[250px] opacity-80 hover:opacity-100 group relative' />
+        <Skeleton className='flex flex-col items-center justify-center w-[450px] h-[250px] opacity-80 hover:opacity-100 group relative' />
+        <Skeleton className='flex flex-col items-center justify-center w-[450px] h-[250px] opacity-80 hover:opacity-100 group relative' />
+        <Skeleton className='flex flex-col items-center justify-center w-[450px] h-[250px] opacity-80 hover:opacity-100 group relative' />
+      </div>
+    </>
+  )
 }
 export default Loading
