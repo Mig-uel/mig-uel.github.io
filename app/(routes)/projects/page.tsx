25b1071567d486bf9615f41a0c9e1af55ecbd1a3
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-24 20:00:16 -0400

    Add ProjectsContainer to ProjectsPage

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,4 +1,14 @@
+import ProjectsContainer from '@/components/projects/projects-container.component'
+
 const ProjectsPage = () => {
-  return <div>ProjectsPage</div>
+  return (
+    <>
+      <p className='mt-10 text-center text-muted-foreground'>
+        These are all of my projects that I've worked on throughout my learning
+        experience.
+      </p>
+      <ProjectsContainer />
+    </>
+  )
 }
 export default ProjectsPage
