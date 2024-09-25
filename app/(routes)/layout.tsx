bd6d912866c9ec5f41a7a32b2d49cf72b7798b2c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-24 21:59:58 -0400

    Refactor layout in RoutesLayout component

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,16 +1,24 @@
 'use client'
 
 import Title from '@/components/general/title.component'
+import { Home } from 'lucide-react'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname()
 
   return (
-    <section className='flex flex-col items-center py-10'>
-      <Title text={pathname} />
-      {children}
-    </section>
+    <div className='py-10'>
+      <div className='flex items-center justify-center gap-x-2'>
+        <Link href='/'>
+          <Home fill='' size='1.5rem' className='mt-1.5' />
+        </Link>
+        <Title text={pathname} className='pl-0' />
+      </div>
+
+      <section className='flex flex-col items-center'>{children}</section>
+    </div>
   )
 }
 export default RoutesLayout
