04229fc971c784a2d7bece4c83d3091219f1a23b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-12 08:24:04 -0400

    Refactor ProjectsPage component to use a container element for better styling

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -2,13 +2,13 @@ import ProjectsContainer from '@/components/projects/projects-container.componen
 
 const ProjectsPage = () => {
   return (
-    <>
+    <div className='container'>
       <p className='pt-10 text-center text-muted-foreground'>
         These are all of my projects that I've worked on throughout my learning
         experience.
       </p>
       <ProjectsContainer />
-    </>
+    </div>
   )
 }
 export default ProjectsPage
