527dc2abf700ead2f1794b2bf84506f7815570c6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-12 10:54:39 -0400

    Refactor Title component to remove unnecessary padding

--- components/general/title.component.tsx
+++ components/general/title.component.tsx
@@ -1,7 +1,7 @@
 const Title = ({ text, className }: { text: string; className?: string }) => {
   return (
     <h1
-      className={` capitalize text-3xl font-bold pl-5 w-full self-start text-left ${
+      className={` capitalize text-3xl font-bold w-full self-start text-left ${
         className && className
       }`}
     >
