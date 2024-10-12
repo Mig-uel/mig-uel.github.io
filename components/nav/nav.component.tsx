f75dce3ff1a56340fe50ddb5ff4fe2ab94049f93
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-12 15:09:41 -0400

    Refactor Nav component to add back button and adjust title alignment

--- components/nav/nav.component.tsx
+++ components/nav/nav.component.tsx
@@ -1,6 +1,28 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import Title from '../general/title.component'
+import { BiSolidLeftArrow } from 'react-icons/bi'
+import Link from 'next/link'
 
 const Nav = ({ title }: { title: string }) => {
-  return <Title text={title} />
+  const pathname = usePathname()
+
+  const pathnameArray = pathname.split('/')
+
+  const isChild = pathnameArray.length > 2
+
+  const href = isChild ? `/${pathnameArray[1]}` : ''
+
+  return (
+    <div className='flex justify-center items-center gap-x-2'>
+      {isChild && (
+        <Link href={href}>
+          <BiSolidLeftArrow className='inline' />
+        </Link>
+      )}
+      <Title text={title} className='inline' />
+    </div>
+  )
 }
 export default Nav
