9dd157ab48e8b0e4867b5d8049de055c4f993e4f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-12 10:54:19 -0400

    Refactor ProjectsPage component to add navigation

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,8 +1,11 @@
+import Nav from '@/components/nav/nav.component'
 import ProjectsContainer from '@/components/projects/projects-container.component'
 
 const ProjectsPage = () => {
   return (
     <div className='container'>
+      <Nav title='Projects' />
+
       <p className='pt-10 text-center text-muted-foreground'>
         These are all of my projects that I've worked on throughout my learning
         experience.
