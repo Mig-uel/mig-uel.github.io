749b7789721f86ce0ce679ef897d465c13d70135
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-12 07:57:35 -0400

    Refactor ProjectPage component to use async/await for fetching project data

--- app/(routes)/projects/[id]/page.tsx
+++ app/(routes)/projects/[id]/page.tsx
@@ -1,8 +1,35 @@
+import { getProjectById } from '@/firebase/db'
+import Image from 'next/image'
+
 type Params = {
   params: { id: string }
 }
 
-const ProjectPage = ({ params: { id } }: Params) => {
-  return <div>{id}</div>
+interface Project {
+  id: string
+  description: string
+  tags: string[]
+  title: string
+  imageUrl: string
+  demoUrl: string
+}
+
+const ProjectPage = async ({ params: { id } }: Params) => {
+  const project = (await getProjectById(id)) as Project
+
+  console.log(project)
+
+  return (
+    <div className='pt-10 w-full'>
+      <div
+        className='w-full h-[300px]'
+        style={{
+          background: `url(${project.imageUrl})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+        }}
+      ></div>
+    </div>
+  )
 }
 export default ProjectPage
