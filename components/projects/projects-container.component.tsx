27853c07f8a9b817dc879b5ea81ac7b0fab3df5c
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-10-12 08:28:50 -0400

    Refactor ProjectsContainer component to adjust top padding on medium screens

--- components/projects/projects-container.component.tsx
+++ components/projects/projects-container.component.tsx
@@ -14,7 +14,7 @@ const ProjectsContainer = async () => {
   const projects = (await getProjects()) as Project[]
 
   return (
-    <div className='pt-20 grid md:grid-cols-2 lg:grid-cols-3 gap-y-10'>
+    <div className='pt-10 md:pt-20 grid md:grid-cols-2 lg:grid-cols-3 gap-y-10'>
       {projects.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
