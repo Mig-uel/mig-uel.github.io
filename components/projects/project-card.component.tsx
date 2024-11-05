39c5d77e520cd44579cd8818a8326636ade87862
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-05 18:33:01 -0500

    Refactor ProjectCard component for improved responsive behavior and styling

--- components/projects/project-card.component.tsx
+++ components/projects/project-card.component.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Subtitle from '../general/subtitle.component'
 import Link from 'next/link'
 
@@ -16,14 +15,14 @@ const ProjectCard = ({ id, imageUrl, title }: Project) => {
     <>
       <Link href={`/projects/${id}`}>
         <div
-          className='flex flex-col items-center justify-center h-[250px] opacity-80 hover:opacity-100 group relative'
+          className='flex flex-col items-center justify-center h-[250px] md:opacity-80 md:hover:opacity-100 group relative'
           style={{
             background: `url(${imageUrl})`,
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
           }}
         >
-          <div className='absolute hidden group-hover:block'>
+          <div className='md:absolute md:hidden group-hover:block'>
             <Subtitle text={title} className='text-white pt-0' />
           </div>
         </div>
