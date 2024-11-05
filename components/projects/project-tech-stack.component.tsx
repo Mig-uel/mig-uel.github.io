4ebc21d66acb241df7e6671849faa81993802ea7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-05 18:33:17 -0500

    Refactor ProjectTechStack component to use Button for tech icons and improve layout

--- components/projects/project-tech-stack.component.tsx
+++ components/projects/project-tech-stack.component.tsx
@@ -1,14 +1,19 @@
+import { Button } from '../ui/button'
 import ProjectTechIcon from './project-tech-icon.component'
 
 const ProjectTechStack = ({ tags }: { tags: Tag[] }) => {
   return (
     <div
-      className={`p-4 grid gap-y-10 mt-5 ${
+      className={`grid gap-y-10 mt-8 ${
         tags.length > 2 ? 'md:grid-cols-3' : 'md:grid-cols-2'
-      }`}
+      }  place-items-center`}
     >
       {tags.map((tag) => (
-        <ProjectTechIcon key={tag} tag={tag} />
+        <Button asChild key={tag} variant='secondary' className='p-10'>
+          <div className='w-[250px]'>
+            <ProjectTechIcon tag={tag} />
+          </div>
+        </Button>
       ))}
     </div>
   )
