a64405189005a4163bf70ae288956be77e30076b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-05 19:12:39 -0500

    Enhance AboutPage layout and add resume download button

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import { GraduationCap, MapPin } from 'lucide-react'
 import Subtitle from '@/components/general/subtitle.component'
 import Nav from '@/components/nav/nav.component'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { FaRegFilePdf } from 'react-icons/fa6'
 
 const AboutPage = () => {
   return (
@@ -21,7 +24,7 @@ const AboutPage = () => {
         <Subtitle text='Miguel Castillo' />
       </div>
 
-      <div className='flex flex-col md:flex-row gap-3 items-center justify-center capitalize font-normal mt-5 text-gray-500'>
+      <div className='flex gap-3 items-center justify-center capitalize font-normal mt-5 text-gray-500 text-md'>
         <Icon
           icon={<GraduationCap fill='gray' size='1rem' />}
           text='Brooklyn College'
@@ -29,17 +32,33 @@ const AboutPage = () => {
         <Icon icon={<MapPin fill='gray' size='1rem' />} text='Brooklyn' />
       </div>
 
-      <p className='self-start mt-10 w-full px-5 md:px-20 text-center'>
-        Hello, world! I am Miguel, a full-stack developer. I graduated from
-        Brooklyn College with a Bachelors in Computer Science. Since graduating,
-        I've been specializing in full-stack development. I enjoy coding,
-        creating websites, and watching movies.
-      </p>
+      <div className='text-center my-8'>
+        <Button asChild variant='link'>
+          <Link
+            target='_blank'
+            href='https://www.dropbox.com/scl/fi/u16h22v1xynn2ortdbhlg/Miguel-Castillo-Software-Engineer.pdf?rlkey=2dpys3yg1v43t73hmox71zqf1&st=z2mfh1rv&dl=0'
+          >
+            <div className='flex items-center justify-center gap-2'>
+              <FaRegFilePdf />
+              Download Resume
+            </div>
+          </Link>
+        </Button>
+      </div>
 
-      <div className='mt-12 w-full'>
-        <Subtitle text='Experience' className='pl-5' />
+      <div className='p-2 md:p-0'>
+        <p className='self-start  w-full px-5 md:px-20 text-center bg-muted rounded p-2 md:p-4'>
+          I am Miguel, a full-stack developer. I graduated from Brooklyn College
+          with a Bachelors in Computer Science. Since graduating, I've been
+          specializing in full-stack development. I enjoy coding, creating
+          websites, and watching movies.
+        </p>
+      </div>
 
-        <div className='flex flex-col mt-10 px-5 md:px-0 md:items-center'>
+      <div className='mt-10 w-full p-2 md:p-0'>
+        <Subtitle text='Experience' />
+
+        <div className='flex flex-col mt-8 px-5 md:px-0 md:items-center bg-muted rounded p-5'>
           <div className='md:w-[350px] h-fit border-l-4 border-black flex flex-col relative'>
             <div className='pl-4'>
               <p className='font-medium capitalize text-lg'>
@@ -86,10 +105,10 @@ const AboutPage = () => {
         </div>
       </div>
 
-      <div className='mt-12 w-full'>
-        <Subtitle text='Education' className='pl-5' />
+      <div className='mt-10 w-full p-2 md:p-0'>
+        <Subtitle text='Education' />
 
-        <div className='flex flex-col mt-10 px-5 md:px-0 md:items-center'>
+        <div className='flex flex-col mt-10 px-5 md:px-0 md:items-center bg-muted rounded p-5'>
           <div className='md:w-[350px] h-[150px] border-l-4 border-black flex flex-col relative'>
             <div className='pl-4'>
               <p className='font-medium capitalize text-lg'>Brooklyn College</p>
