3bba40f2ea62fd9b00084ec36dac005636bf40f6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-06 18:21:07 -0500

    Add tags utility for common technology identifiers

---
+++ utils/tags.ts
@@ -0,0 +1,14 @@
+export const tags: Tag[] = [
+  'api',
+  'css',
+  'firebase',
+  'html',
+  'javascript',
+  'mongodb',
+  'mongoose',
+  'nextjs',
+  'react',
+  'redux',
+  'styled-components',
+  'typescript',
+]
