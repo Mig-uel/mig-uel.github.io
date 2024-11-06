2b7534fa7cc2041b6d4bff61201101e9d7eeb306
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-11-06 15:47:59 -0500

    Enhance loading component with additional Skeleton elements and responsive grid layout

--- app/(routes)/projects/loading.tsx
+++ app/(routes)/projects/loading.tsx
@@ -3,12 +3,14 @@ import { Skeleton } from '@/components/ui/skeleton'
 const Loading = () => {
   return (
     <div className='container'>
+      <Skeleton className='w-32 h-8 ml-2' />
+
       <div className='w-full mt-10 flex flex-col justify-center items-center gap-y-1'>
         <Skeleton className='min-w-[350px] w-[650px]  h-6' />
         <Skeleton className='min-w-[250px] w-[550px]  h-6' />
       </div>
 
-      <div className='mt-20 grid md:grid-cols-3 gap-y-10 gap-x-2 justify-center'>
+      <div className='mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-center'>
         <Skeleton className='flex flex-col items-center justify-center h-[250px] opacity-80 hover:opacity-100 group relative' />
         <Skeleton className='flex flex-col items-center justify-center h-[250px] opacity-80 hover:opacity-100 group relative' />
         <Skeleton className='flex flex-col items-center justify-center h-[250px] opacity-80 hover:opacity-100 group relative' />
