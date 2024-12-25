126bd8f5eaf050b4dd96dad0f38477d8ffb6aae1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-12-24 20:08:21 -0500

    Add Skills component to AboutPage for showcasing technical and soft skills

---
+++ components/about/skills.component.tsx
@@ -0,0 +1,65 @@
+import Subtitle from '../general/subtitle.component'
+
+type SkillsType = string[]
+
+const Skill = ({ skill }: { skill: string }) => {
+  return <span className='rounded-xl bg-muted  px-4'>{skill}</span>
+}
+
+const Skills = () => {
+  const technicalSkills: SkillsType = [
+    'HTML/CSS',
+    'JavaScript',
+    'React',
+    'Node.js',
+    'Express',
+    'SQL',
+    'NoSQL',
+    'ORM/ODM',
+    'RESTful API Development',
+    'GraphQL',
+    'JSON Web Token (JWT)',
+    'Docker',
+    'Tailwind CSS',
+    'Jest',
+    'Git/GitHub',
+    'Responsive Design',
+    'Mobile-First Development',
+  ]
+
+  const softSkills: SkillsType = [
+    'Customer Engagement',
+    'Inventory Optimization',
+    'Problem-Solving',
+    'Analytical Thinking',
+    'Collaboration',
+    'Communication',
+    'Time-Management',
+    'Adaptability',
+  ]
+
+  return (
+    <>
+      <div>
+        <Subtitle text='Technical Skills' />
+
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {technicalSkills.map((skill) => (
+            <Skill skill={skill} />
+          ))}
+        </div>
+      </div>
+
+      <div className='mt-10'>
+        <Subtitle text='Soft Skills' />
+
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {softSkills.map((skill) => (
+            <Skill skill={skill} />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+export default Skills
