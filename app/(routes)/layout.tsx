ee448a8bc3e3ea9dff662e4757223c3ed510b030
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-01-30 16:53:53 -0500

    delete files

--- app/(routes)/layout.tsx
+++
@@ -1,8 +0,0 @@
-const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='py-10'>
-      <section className='container'>{children}</section>
-    </div>
-  )
-}
-export default RoutesLayout
--- components/general/footer.component.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-  const year = new Date().getFullYear()
-  return (
-    <footer className='text-center pt-5'>&copy; Miguel Castillo {year}</footer>
-  )
-}
-export default Footer
