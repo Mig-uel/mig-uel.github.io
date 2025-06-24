5aa637e43972a6a1fdce52c743adaa9572e3bd28
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-06-23 22:52:34 -0400

    feat: replace static text with animated text components
    - Enhanced the HomePage by integrating AnimatedText for improved
      visual appeal and animation effects.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import SocialButtons from '@/components/social-buttons.component'
+import AnimatedText from '../components/motion/animated-text'
 
 export default function HomePage() {
   return (
     <>
       <section className='h-screen flex flex-col justify-center items-center text-center gap-y-10'>
         <div className='flex flex-col gap-4 w-fit h-fit'>
-          <h1 className='text-4xl md:text-6xl font-bold'>Miguel Castillo</h1>
-          <p className='capitalize font-medium tracking-wide text-muted-foreground'>
+          <AnimatedText className='text-4xl md:text-6xl font-bold'>
+            Miguel Castillo
+          </AnimatedText>
+          <AnimatedText
+            className='capitalize font-medium tracking-wide text-muted-foreground'
+            tag='p'
+          >
             Full Stack Developer
-          </p>
+          </AnimatedText>
         </div>
 
         <SocialButtons />
